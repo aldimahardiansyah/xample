a49b3f76971f69944771e5d98efd91838fdd9cf6
aldimdiansyah <aldimdiansyah@gmail.com>
2020-11-13 07:14:17 +0700

    Tugas kuliah. Commit on code

---
+++ DDP/index.py
@@ -0,0 +1,7 @@
+def tambah(a, b, c):
+    hasil = a+b+c
+    return hasil
+
+
+x = tambah(5, 7, 9)
+print(x)
---
+++ P.Web/Praktikum-4/script.js
@@ -0,0 +1,8 @@
+let angka = prompt("Masukkan angka: ");
+
+for (let i = 0; i <= angka; i++) {
+    console.log("#");
+    for (let j = 0; j <= angka; i++) {
+        console.log("$");
+    }
+}
